6499a7685973025c72e7c8ab5e00acbaf6992e67
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-168-122-2-166.bu.edu>
2018-11-10 17:37:21 -0500

    done parsing

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var express = require('express')
+var client = require('twilio')(
+  'ACa9129633d27ef3500b23dfae414977cf',
+  'p68fb77df16d1defb3bb9744da131eed1'
+);
+const axios = require ('axios')
+const request = require ('request')
+
+const superagent = require('superagent')
 const https = require('https')
 const bodyParser = require('body-parser')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
@@ -21,7 +29,40 @@ app.post('/receive', function (req, res) {
   console.log(req.body.Body);
   var params = req.body.Body.split(" ");
 
-  if(params[0] == "va_")
+  if(params[0] == "va_assist"){
+    console.log("now make http request");
+
+    var APIKEY = "BGZMwn7elWRPHlxknLOYEFVU3bP2RWqD";
+    var long = 42.349416;
+    var lat = -71.106412;
+
+
+    var options = { method: 'GET',
+    url: 'https://dev-api.va.gov/services/va_facilities/v0/facilities',
+    qs: 
+     { apikey: 'BGZMwn7elWRPHlxknLOYEFVU3bP2RWqD',
+       long: '42.349416',
+       lat: '-71.106412' }
+    };
+  
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      var result = JSON.parse(body)
+      var textmsg = "Nearby VA facilities are as follows \n"
+      for(let i = 0 ; i < 5 ; i++){
+        textmsg +=  result.data[0].attributes.name +"\n"
+      }
+
+      client.messages.create({
+        from: 8572038387,
+        to: 7819520510,
+        body: textmsg
+      }).then((message) => console.log(message.sid));
+      //console.log(body.data[0].attributes.name);
+    });
+
+
+  }
 })
 
 app.listen(3000, function () {
